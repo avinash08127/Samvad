0f3af9a71cd174db0a5ebeeb9d5f77c744a18459
AvinashKumar Mishra <avinashkumar.mishra@emtecinc.com>
2020-10-24 15:30:17 +0530

    added routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,13 +12,16 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegistrationMode = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService : AccountService) { }
+  constructor(private accountService : AccountService, private toastr : ToastrService) { }
 
   ngOnInit() {
   }
   register(){
     this.accountService.register(this.model).subscribe(response => {
       this.cancel();
+    },error => {
+      console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
